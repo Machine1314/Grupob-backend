be4fd6359257d38894916503e31c362ff9672e85
Bruno Dueñas <bruno.duenas@udla.edu.ec>
2021-04-23 21:21:46 -0500

    Se agrega carpeta assets con etiquetas de los menus del grupo

---
+++ assets/menu-B-en_US.js
@@ -0,0 +1,9 @@
+{
+    "COMMONS": {
+        "MENU": {
+            "MNU_BTEST2": "Invoices by ID",
+            "MNU_BTEST1": "Invoices by date",
+            "MNU_BTEST": "Group B"
+        }
+    }
+}
---
+++ assets/menu-B-es_EC.js
@@ -0,0 +1,9 @@
+{
+    "COMMONS": {
+        "MENU": {
+            "MNU_BTEST2": "Facturas por Cedula",
+            "MNU_BTEST1": "Facturas por Fechas",
+            "MNU_BTEST": "Grupo B"
+        }
+    }
+}
