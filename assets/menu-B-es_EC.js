6ddadd2bcc3da87c7184312aabeab75668f3f902
Douglas Gallardo <61000120+DouglasGallardoZ@users.noreply.github.com>
2021-04-25 00:50:56 -0500

    Update menu-B-es_EC.js

--- assets/menu-B-es_EC.js
+++ assets/menu-B-es_EC.js
@@ -3,7 +3,8 @@
         "MENU": {
             "MNU_BTEST2": "Facturas por Cedula",
             "MNU_BTEST1": "Facturas por Fechas",
-            "MNU_BTEST": "Grupo B"
+            "MNU_BTEST": "Grupo B",
+            "MNU_BTEST_CLIENTE":"Clientes"
         }
     }
 }
